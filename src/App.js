b776092ebff6e18a9b593775b16a59b3745d5e45
Fábio Mateus Melo <fabioceto@gmail.com>
2022-07-16 19:54:23 -0300

    0002

--- src/App.js
+++ src/App.js
@@ -5,18 +5,26 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [emotion, setEmotion] = useState("happy");
+  const [secondary, setSecondary] = useState("tired");
 
   useEffect(() => {
-    console.log(`Essa é uma emoção ${emotion}`);
-  }, [emotion]);
+    console.log(`Essa é a emoção ${emotion}`);
+  }, [emotion ]);
+  useEffect(() => {
+    console.log(`Essa é a emoção ${secondary}`);
+  }, [secondary]);
 
   return (
     <div className="App">
-      <h1>Current emotion is {emotion}</h1>
+      <h1>A emoção primária é {emotion}</h1>
       <button onClick={() => setEmotion("sad")}>Sad
       </button>
       <button onClick={() => setEmotion("excited")}>Excited
       </button>
+        <h2>
+          A emoção secundária é {secondary}.
+        </h2>
+        <button onClick={() => setSecondary("grateful")}>Grateful</button>
     </div>
   );
 }
